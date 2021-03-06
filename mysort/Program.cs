f73534711d933efa71a5bcab8d26f0657387ce15
Andy Collins <askingalot@gmail.com>
2021-03-06 15:23:31 -0600

    mysorted

---
+++ mysort/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+if (args.Length != 1)
+{
+    Console.Error.WriteLine("USAGE: mysort <filename>");
+    Environment.Exit(1);
+}
+
+var filename = args[0];
+
+if (!File.Exists(filename))
+{
+    Console.Error.WriteLine($"No such file: {filename}");
+    Environment.Exit(1);
+}
+
+var sorted = File.ReadAllLines(filename).OrderBy(line => line);
+foreach (var line in sorted)
+{
+    Console.WriteLine(line);
+}
