aa8d36388208567a42793bf25b2b560cb624844f
Andy Collins <askingalot@gmail.com>
2021-03-06 10:26:04 -0600

    mycat and mytail

---
+++ mycat/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+const int BUFFER_SIZE = 4096;
+
+if (args.Length == 0)
+{
+    Console.Error.WriteLine("USAGE: mycat <filename> ...");
+    Environment.Exit(1);
+}
+
+var buffer = new char[BUFFER_SIZE];
+
+foreach (var filename in args)
+{
+    if (!File.Exists(filename))
+    {
+        Console.Error.WriteLine("No such file: " + filename);
+        continue;
+    }
+
+    using var reader = new StreamReader(filename);
+    int numRead;
+    while ((numRead = reader.Read(buffer, 0, BUFFER_SIZE)) != 0)
+    {
+        Console.Write(buffer, 0, numRead);
+    }
+}
---
+++ mytail/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+const int LINE_COUNT = 10;
+
+if (args.Length != 1)
+{
+    Console.Error.WriteLine("USAGE: mytail <filename>");
+    Environment.Exit(1);
+}
+
+var filename = args[0];
+
+if (!File.Exists(filename))
+{
+    Console.Error.WriteLine($"No such file: {filename}");
+    Environment.Exit(1);
+}
+
+var queue = new FixedStringQueue(LINE_COUNT);
+string line;
+
+using (var reader = new StreamReader(filename))
+{
+    while ((line = reader.ReadLine()) != null)
+    {
+        queue.Enqueue(line);
+    }
+}
+
+while ((line = queue.Dequeue()) != null)
+{
+    Console.WriteLine(line);
+}
+
+
+class FixedStringQueue
+{
+    private int _maxSize;
+    private Queue<string> _internalQueue;
+
+    public FixedStringQueue(int maxSize)
+    {
+        if (maxSize <= 0)
+        {
+            throw new ArgumentException($"{nameof(maxSize)} must be greater than zero.");
+        }
+        _maxSize = maxSize;
+        _internalQueue = new(maxSize);
+    }
+
+    public void Enqueue(string newValue)
+    {
+        if (_internalQueue.Count == _maxSize)
+        {
+            _internalQueue.Dequeue();
+        }
+        _internalQueue.Enqueue(newValue);
+    }
+
+    public string Dequeue()
+    {
+        _internalQueue.TryDequeue(out var value);
+        return value;
+    }
+}
